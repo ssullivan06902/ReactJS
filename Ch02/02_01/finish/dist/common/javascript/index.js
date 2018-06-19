6cd08ba580916cfd75aa1840854c040fc8a8eae7
Sean Sullivan <sean.m.a.sullivan@icloud.com>
2018-06-19 11:14:35 -0400

    Created my first reactJS element

--- Ch02/02_01/finish/dist/common/javascript/index.js
+++ Ch02/02_01/finish/dist/common/javascript/index.js
@@ -4,10 +4,21 @@ const { render } = ReactDOM
 const title = createElement(
 	'h1',
 	{id: 'title', className: 'header'},
-	'Hello World! These are the voyages of the starship Enterprise!'
+	'Hello World!'
 )
 
 render(
 	title,
 	document.getElementById('react-container')
+)
+
+const subtitle = createElement(
+	'h2',
+	{id:'subtitle', className: 'header'},
+	'These are the voyages of the starship Enterprise!'
+)
+
+render(
+	subtitle,
+	document.getElementById('react-subtitle')
 )
