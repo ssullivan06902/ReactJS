32c42f75d0b8ff6390a7042a8b9070ae8a1a3663
Sean Sullivan <sean.m.a.sullivan@icloud.com>
2018-06-20 10:25:31 -0400

    updates made to sub-title feature

--- Ch02/02_04/finish/dist/bundle.js
+++ Ch02/02_04/finish/dist/bundle.js
@@ -9,5 +9,13 @@ render(React.createElement(
 	{ id: 'title',
 		className: 'header',
 		style: { backgroundColor: 'orange', color: 'white', fontFamily: 'verdana' } },
-	'Hello World'
+	'Hello World!'
 ), document.getElementById('react-container'));
+
+render(React.createElement(
+	'h2',
+	{ id: 'title',
+		className: 'header',
+		style: { backgroundColor: 'blue', color: 'white', fontFamily: 'verdana' } },
+	'These are the voyages of the starship Enterprise!'
+), document.getElementById('react-subtitle'));
--- Ch02/02_04/finish/src/index.js
+++ Ch02/02_04/finish/src/index.js
@@ -4,7 +4,16 @@ render(
 	<h1 id='title'
 		className='header'
 		style={{backgroundColor: 'orange', color: 'white', fontFamily: 'verdana'}}>
-	Hello World	
+	Hello World!
+	</h1>,
+	document.getElementById('react-container')
+)
+
+render(
+	<h2 id='title'
+		className='header'
+		style={{backgroundColor: 'blue', color: 'white', fontFamily: 'verdana'}}>
+	These are the voyages of the starship Enterprise!
 	</h1>,
 	document.getElementById('react-container')
 )
